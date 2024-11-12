545163f401a602ef53d7d3d520ee0b7c04025855
somesh <AquaQASomesh@gmail.com>
2024-11-12 20:37:36 +0530

    first commit

---
+++ src/test/java/TESTNG/class01.java
@@ -0,0 +1,13 @@
+package TESTNG;
+
+import org.testng.annotations.Test;
+
+public class class01
+{
+    @Test
+    public static void test01()
+        {
+
+        System.out.println("hello");
+    }
+}
